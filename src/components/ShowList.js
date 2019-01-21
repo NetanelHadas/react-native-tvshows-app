0ea06567c4b7d8d3f2d1d99e693229130044b899
DESKTOP-CT28P5M\netan <netanel.hadas@gmail.com>
2019-01-21 12:42:43 +0200

    axios http request finished

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -3,15 +3,25 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class ShowList extends Component {
+    state = { shows: [] };
+    
     componentWillMount() {
-        axios.get("http://api.tvmaze.com/search/shows?q=girls")
-            .then(response => console.log(response));
+        axios.get('http://api.tvmaze.com/search/shows?q=girls')
+            .then(response => this.setState({ shows: response.data }));
+    }
+
+    renderShows() {
+        return this.state.shows.map(show =>
+            <Text key={show.show.id}>{show.score}</Text>
+        );
     }
     
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Show list</Text>
+                {this.renderShows()}
             </View>
         );
     };
